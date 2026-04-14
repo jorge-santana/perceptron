b4adf2ef1f3a1e1a69a0b92d4a0294c125e94691
Jorge Sant Ana <jorge@argus-academy.com>
2026-04-14 13:12:01 -0300

    feat: inicia definição da classe do perceptron com os métodos __init__, treinar e prever

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import numpy as np
 
 # Problema: Baseado em peso e altura, quero fazer a predição se é adulto ou criança
 
-# Dados de entrada (inputs, features) - treinamento / predição
+class Perceptron:
+    def __init__(self):
+        print("Instância da classe Perceptron > Objeto Perceptron")
 
+    def treinar(self):
+        print("Treinar o modelo")
+
+    def prever(self):
+        print("Fazer previsões com base no treinamento")
+
+modelo = Perceptron()
+modelo.treinar()
+modelo.prever()
+
+# Dados de entrada (inputs, features) - treinamento / predição
 # [peso (kg), altura (m)]
 entradas = np.array([
     [15, 1.0], # criança
@@ -11,11 +24,9 @@ entradas = np.array([
     [65, 1.7], # adulto
     [80, 1.8], # adulto
 ])
-
-print("Dados de entrada: ", entradas)
+# print("Dados de entrada: ", entradas)
 
 # Rótulos (Labels): Treinamento supervisionado
 # 0 = criança; 1 = adulto
 rotulos = np.array([0, 0, 1, 1])
-
-print("Dados de validação (treinamento): ", rotulos)
+# print("Dados de validação (treinamento): ", rotulos)
